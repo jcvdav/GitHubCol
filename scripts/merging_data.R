16363fd8c0454deb4e24b6d1b7eeda8d86ec1eee
jcvdav <juancarlos.villader@gmail.com>
2019-03-12 12:19:41 -0700

    Merged old and new data

---
+++ scripts/merging_data.R
@@ -0,0 +1,14 @@
+# Merging my datasets
+# Read old data
+
+library(here)
+
+old_data <- read.csv(here("raw_data", "iris.csv"))
+new_data <- read.csv(here("raw_data", "iris_mythica.csv"))
+
+# Put them together
+data <- rbind(old_data, new_data)
+
+
+# Save them
+write.csv(x = data, file = here("data", "iris_four.csv"))
